48a98b1576b97a7e35b624f3188c477bb2740c09
yogishaaniraula <137094510+yogishaaniraula@users.noreply.github.com>
2024-04-16 15:38:42 +0000

    DataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat al JS code as newer version
+
+// alert ("3+3") // we are using node js not browser
+
+console.log(3+3)
+ // code readability should be high
+console.log("yogisha")
+
+let name = "yogisha"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true /false
+// null =>standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "yogisha");
