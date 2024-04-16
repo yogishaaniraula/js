221ee6ee4b84c94eab842a68aad5926cca6d4ff6
yogishaaniraula <137094510+yogishaaniraula@users.noreply.github.com>
2024-04-16 15:38:27 +0000

    variables

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "yogishagoogle.com"
+var accountPassword = "12345"
+accountCity = "kathmandu"
+
+// accountId = 2 // not allowed
+
+accountEmail = "DJfo@.com"
+accountPassword = "23452"
+accountCity = "lalitpur" 
+
+console.log(accountId);
+console.table ( [accountId,accountEmail,accountPassword,accountCity])
