991d6f06104815ac16513d6cedb738df31155b0e
yogishaaniraula <137094510+yogishaaniraula@users.noreply.github.com>
2024-04-28 06:37:53 +0000

    datatypes

---
+++ 01_Basics/dataTypes summary.js
@@ -0,0 +1,34 @@
+// primitive data type:
+ 
+ 
+// 7 types : string , number , boolean , null , undefined , symbol , BigInt
+
+
+const score = 100
+const scoreValule =100.3
+
+const isLoggedIn = false 
+const outsideTemp = null 
+let userEmail; // (undefined)
+
+const id = Symbol ("123")  
+const anotherId = Symbol("123")
+
+console.log ( id === anotherId); 
+// reference data type( non primitive):
+// array , objects , functions 
+
+const herso = {"shaktiman","doga"}
+  let myObj = {
+        name : "yogisha",
+        age : 20,
+    }
+
+const myFunction = function ()
+{
+console.log("hello world");
+}
+
+console.log(typeof outsideTemp);
+
+ 
