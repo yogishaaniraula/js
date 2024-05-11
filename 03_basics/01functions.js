601ed231f667dcf85d5f1635e28328842bcf9628
yogishaaniraula <137094510+yogishaaniraula@users.noreply.github.com>
2024-05-11 03:12:34 +0000

    functions

---
+++ 03_basics/01functions.js
@@ -0,0 +1,49 @@
+
+
+function sayMyName()
+{
+    console.log("h")
+console.log("e")
+console.log("l")
+console.log("l")
+console.log("o")
+}
+
+//sayMyName()
+
+
+//  function addTwonumbers (num1,num2)
+// {
+//     console.log(num1 + num2);
+
+// }
+
+
+function addTwonumbers (num1,num2)
+{
+let result = num1 + num2
+return result
+return num1 + num2
+}
+
+const result = addTwonumbers(3, 6)
+
+//console.log("result:", result);
+
+
+function loginUsermessage(username)
+{
+    return`${username} just logged in`
+}
+
+loginUsermessage("yogisha")
+
+
+
+
+
+
+
+
+
+
