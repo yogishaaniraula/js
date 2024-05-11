3557d7a155e6e388ca0d244b2313a77d8265e5c2
yogishaaniraula <137094510+yogishaaniraula@users.noreply.github.com>
2024-05-11 09:24:00 +0000

    fun

---
+++ 03_basics/scope.js
@@ -0,0 +1,32 @@
+// var c = 300
+
+//let a = 300
+
+
+if (true) {
+    let a = 10
+const b =20
+console.log("Inner : ",a)
+ 
+}
+
+for (let i = 0; i<Array.length; )
+
+
+console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+
+function one() {
+    const username= "yogisha"
+
+    function two(){
+        const website="youtube";
+    }
+    console.log(website);
+
+    two ();
+}
+
